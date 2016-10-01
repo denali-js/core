169f008bc2769d88286a3753ba1b800b3d0d2d6f
Dave Wasmer <davewasmer@gmail.com>
2016-09-30 23:32:12 -0600

    chore: add some logging to docs build

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,43 @@
 
 set -e
 
-tmpdir=`mktemp -d`
-node_modules_backup=`mktemp -d`
-
 if [ -n "$(git status --porcelain)" ]; then
   echo "You have uncommitted changes - please commit or stash before deploying."
   exit 1
 fi
 
+echo "Creating tmp directories to work with"
+tmpdir=`mktemp -d`
+node_modules_backup=`mktemp -d`
+
+echo "Cleaning up previous builds"
 rm -rf dist
+
+echo "Building"
 NODE_ENV=production node_modules/.bin/broccoli build $tmpdir/dist
 
+echo "Backing up node_modules"
 mv node_modules $node_modules_backup/
 
+echo "Checking out gh-pages branch"
 git checkout gh-pages
+
+echo "Cleaning gh-pages branch"
 rm -rf ./*
+
+echo "Copying over build result"
 cp -r $tmpdir/dist/ ./
+
+echo "Updating gh-pages branch with new build"
 git add .
 git commit -m 'update docs'
-git push origin gh-pages
-git checkout docs
 
+echo "Pushing gh-pages branch"
+git push origin gh-pages
+
+echo "Restoring docs branch and node_modules"
+git checkout docs
 mv $node_modules_backup/node_modules ./
 
+echo "Cleaning up tmp directories"
 rm -rf $tmpdir $node_modules_backup
