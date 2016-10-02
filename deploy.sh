01f091ae84b4150bc69c01ccc8b62d104df6bf95
Dave Wasmer <davewasmer@gmail.com>
2016-10-01 22:57:52 -0600

    try a more robust docs deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,43 +2,63 @@
 
 set -e
 
-if [ -n "$(git status --porcelain)" ]; then
-  echo "You have uncommitted changes - please commit or stash before deploying."
-  exit 1
+# Adapted from https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+SOURCE_BRANCH="docs"
+TARGET_BRANCH="gh-pages"
+
+function doCompile {
+  NODE_ENV=production node_modules/.bin/broccoli build dist
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping docs deploy."
+    exit 0
 fi
 
-echo "Creating tmp directories to work with"
-tmpdir=`mktemp -d`
-node_modules_backup=`mktemp -d`
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
 
-echo "Cleaning up previous builds"
-rm -rf dist
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO dist
+cd dist
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
 
-echo "Building"
-NODE_ENV=production node_modules/.bin/broccoli build $tmpdir/dist
+# Clean out existing contents
+rm -rf dist/**/* || exit 0
 
-echo "Backing up node_modules"
-mv node_modules $node_modules_backup/
+# Run our compile script
+doCompile
 
-echo "Checking out gh-pages branch"
-git checkout gh-pages
+# Now let's go have some fun with the cloned repo
+cd dist
+git config user.name "Denali CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-echo "Cleaning gh-pages branch"
-rm -rf ./*
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No docs changes on this push; exiting."
+    exit 0
+fi
 
-echo "Copying over build result"
-cp -r $tmpdir/dist/ ./
-
-echo "Updating gh-pages branch with new build"
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
 git add .
-git -c user.name='Denali CI' -c user.email='davewasmer@gmail.com' commit -m 'update docs'
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-echo "Pushing gh-pages branch"
-git push origin gh-pages
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
-echo "Restoring docs branch and node_modules"
-git checkout docs
-mv $node_modules_backup/node_modules ./
-
-echo "Cleaning up tmp directories"
-rm -rf $tmpdir $node_modules_backup
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
