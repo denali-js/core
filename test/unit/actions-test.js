cd9ecd871c0fcc826f50a2db2ff3a76fbc1811d0
Dave Wasmer <davewasmer@gmail.com>
2016-10-22 00:34:24 -0600

    test: convert unit tests to ava

--- test/unit/actions-test.js
+++ test/unit/actions-test.js
@@ -1,9 +1,9 @@
-import expect from 'must';
-import Action from '../../lib/runtime/action';
-import Model from '../../lib/data/model';
-import Response from '../../lib/runtime/response';
-import Container from '../../lib/runtime/container';
-import FlatSerializer from '../../lib/runtime/base/app/serializers/flat';
+import test from 'ava';
+import {
+  Action,
+  Model,
+  Container,
+  FlatSerializer } from 'denali';
 import merge from 'lodash/merge';
 
 function mockReqRes(overrides) {
@@ -34,312 +34,222 @@ function mockReqRes(overrides) {
   }, overrides);
 }
 
-describe('Denali.Action', function() {
-  it('should invoke respond() with params', function() {
-    let responded = false;
-    class TestAction extends Action {
-      respond(params) {
-        expect(params.query).to.be.true();
-        expect(params.body).to.be.true();
-        responded = true;
+test('Action > invokes respond() with params', async (t) => {
+  t.plan(3);
+  class TestAction extends Action {
+    respond(params) {
+      t.true(params.query);
+      t.true(params.body);
+      t.pass();
+    }
+  }
+  let action = new TestAction(mockReqRes({
+    request: {
+      query: { query: true },
+      body: { body: true }
+    }
+  }));
+  return action.run();
+});
+
+test('Action > does not invoke the serializer if no response body was provided', async (t) => {
+  t.plan(1);
+  class TestAction extends Action {
+    serializer = 'foo';
+    respond() {
+      t.pass();
+    }
+  }
+  let mock = mockReqRes();
+  mock.container.register('serializer:foo', {
+    serialize() {
+      t.fail('Serializer should not be invoked');
+    }
+  });
+  let action = new TestAction(mock);
+
+  return action.run();
+});
+
+test('Action > uses a specified serializer type when provided', async (t) => {
+  t.plan(2);
+  class TestAction extends Action {
+    serializer = 'foo';
+    respond() {
+      t.pass();
+      return {};
+    }
+  }
+  let mock = mockReqRes();
+  mock.container.register('serializer:foo', {
+    serialize() {
+      t.pass();
+    }
+  });
+  let action = new TestAction(mock);
+
+  return action.run();
+});
+
+test('Action > renders with the error serializer if an error was rendered', async (t) => {
+  t.plan(2);
+  class TestAction extends Action {
+    respond() {
+      t.pass();
+      return new Error();
+    }
+  }
+  let mock = mockReqRes();
+  mock.container.register('serializer:error', {
+    serialize() {
+      t.pass();
+    }
+  });
+  let action = new TestAction(mock);
+
+  return action.run();
+});
+
+test('Action > should render with the model type serializer if a model was rendered', async (t) => {
+  t.plan(2);
+  let mock = mockReqRes();
+  class TestAction extends Action {
+    respond() {
+      t.pass();
+      return new Proxy({
+        constructor: { type: 'foo' }
+      }, {
+        getPrototypeOf() {
+          return Model.prototype;
+        }
+      });
+    }
+  }
+  mock.container.register('serializer:foo', {
+    serialize() {
+      t.pass();
+    }
+  });
+  let action = new TestAction(mock);
+
+  return action.run();
+});
+
+test('Action > filters > invokes before filters prior to respond()', async (t) => {
+  let sequence = [];
+  class TestAction extends Action {
+    static before = [ 'before' ];
+    static after = [ 'after' ];
+
+    before() {
+      sequence.push('before');
+    }
+
+    respond() {
+      sequence.push('respond');
+    }
+
+    after() {
+      sequence.push('after');
+    }
+  }
+  let action = new TestAction(mockReqRes());
+
+  await action.run();
+  t.deepEqual(sequence, [ 'before', 'respond', 'after' ]);
+});
+
+test('Action > filters > invokes superclass filters before subclass filters', async (t) => {
+  let sequence = [];
+  class ParentClass extends Action {
+    static before = [ 'before' ];
+
+    before() {
+      sequence.push('parent');
+    }
+
+    respond() {}
+  }
+  class ChildClass extends ParentClass {
+    static before = [ 'before', 'beforeChild' ];
+
+    beforeChild() {
+      sequence.push('child');
+    }
+  }
+  let action = new ChildClass(mockReqRes());
+
+  await action.run();
+  t.deepEqual(sequence, [ 'parent', 'child' ]);
+});
+
+test('Action > filters > error out when an non-existent filter was specified', async (t) => {
+  class TestAction extends Action {
+    static before = [ 'some-non-existent-method' ];
+    respond() {}
+  }
+  let action = new TestAction(mockReqRes());
+
+  t.throws(action.run.bind(action));
+});
+
+test('Action > filters > should render the returned value of a filter (if that value != null)', async (t) => {
+  t.plan(1);
+  class TestAction extends Action {
+    static before = [ 'preempt' ];
+    respond() {
+      t.fail('Filter should have preempted this responder method');
+    }
+    preempt() {
+      return { hello: 'world' };
+    }
+    render(result) {
+      t.deepEqual(result, { hello: 'world' });
+    }
+  }
+  let action = new TestAction(mockReqRes());
+
+  return action.run();
+});
+
+test('Action > content negotiation > respond with the content-type specific responder', async (t) => {
+  class TestAction extends Action {
+    respond() {
+      t.fail('Should have used HTML responder');
+    }
+    respondWithHtml() {
+      t.pass();
+    }
+}
+  let action = new TestAction(mockReqRes({
+    request: {
+      headers: {
+        'Content-type': 'text/html'
+      },
+      accepts() {
+        return 'html';
       }
     }
-    let action = new TestAction(mockReqRes({
-      request: {
-        query: { query: true },
-        body: { body: true }
-      }
-    }));
+  }));
 
-    return action.run().then(() => {
-      expect(responded).to.be.true();
-    });
-  });
+  return action.run();
+});
 
-  it('should not invoke the serializer if no response body was provided', function() {
-    let responded = false;
-    let serialized = false;
-    class TestAction extends Action {
-      serializer = 'foo';
-      respond() {
-        responded = true;
-      }
-    }
-    let mock = mockReqRes();
-    mock.container.register('serializer:foo', {
-      serialize() {
-        serialized = true;
-      }
-    });
-    let action = new TestAction(mock);
+test('Action > #modelFor(type) > returns the model for a given type', async (t) => {
+  let mock = mockReqRes();
+  mock.container.register('model:user', { type: 'user' });
+  let action = new Action(mock);
 
-    return action.run().then(() => {
-      expect(responded).to.be.true();
-      expect(serialized).to.be.false();
-    });
-  });
+  let User = action.modelFor('user');
+  t.is(User.type, 'user');
+  t.is(User.container, action.container);
+});
 
-  it('should use the specified serializer type if provided', function() {
-    let responded = false;
-    let serialized = false;
-    class TestAction extends Action {
-      serializer = 'foo';
-      respond() {
-        responded = true;
-        return {};
-      }
-    }
-    let mock = mockReqRes();
-    mock.container.register('serializer:foo', {
-      serialize() {
-        serialized = true;
-      }
-    });
-    let action = new TestAction(mock);
+test('Action > #service(name) > returns the service for a given service name', async (t) => {
+  let mock = mockReqRes();
+  mock.container.register('service:mine', { type: 'mine' });
+  let action = new Action(mock);
 
-    return action.run().then(() => {
-      expect(responded).to.be.true();
-      expect(serialized).to.be.true();
-    });
-  });
-
-  it('should render with the error serializer if an error was rendered', function() {
-    let responded = false;
-    let serialized = false;
-    class TestAction extends Action {
-      respond() {
-        responded = true;
-        return new Error();
-      }
-    }
-    let mock = mockReqRes();
-    mock.container.register('serializer:error', {
-      serialize() {
-        serialized = true;
-      }
-    });
-    let action = new TestAction(mock);
-
-    return action.run().then(() => {
-      expect(responded).to.be.true();
-      expect(serialized).to.be.true();
-    });
-  });
-
-  it('should render with the model type serializer if a model was rendered', function() {
-    let responded = false;
-    let serialized = false;
-    let mock = mockReqRes();
-
-    class TestAction extends Action {
-      respond() {
-        responded = true;
-        return new Proxy({
-          constructor: { type: 'foo' }
-        }, {
-          getPrototypeOf() {
-            return Model.prototype;
-          }
-        });
-      }
-    }
-    mock.container.register('serializer:foo', {
-      serialize() {
-        serialized = true;
-      }
-    });
-    let action = new TestAction(mock);
-
-    return action.run().then(() => {
-      expect(responded).to.be.true();
-      expect(serialized).to.be.true();
-    });
-  });
-
-  describe('filters', function() {
-
-    it('should invoke before filters prior to respond()', function() {
-      let sequence = [];
-      class TestAction extends Action {
-        static before = [ 'before' ];
-        static after = [ 'after' ];
-
-        before() {
-          sequence.push('before');
-        }
-
-        respond() {
-          sequence.push('respond');
-        }
-
-        after() {
-          sequence.push('after');
-        }
-      }
-      let action = new TestAction(mockReqRes());
-
-      return action.run().then(() => {
-        expect(sequence).to.eql([ 'before', 'respond', 'after' ]);
-      });
-    });
-
-    it('should invoke superclass filters before subclass filters', function() {
-      let sequence = [];
-      class ParentClass extends Action {
-        static before = [ 'before' ];
-
-        before() {
-          sequence.push('parent');
-        }
-
-        respond() {}
-      }
-      class ChildClass extends ParentClass {
-        static before = [ 'before', 'beforeChild' ];
-
-        beforeChild() {
-          sequence.push('child');
-        }
-      }
-      let action = new ChildClass(mockReqRes());
-
-      return action.run().then(() => {
-        expect(sequence).to.eql([ 'parent', 'child' ]);
-      });
-    });
-
-    it('should error out when an non-existent filter was specified', function() {
-      class TestAction extends Action {
-        static before = [ 'some-non-existent-method' ];
-        respond() {}
-      }
-      let action = new TestAction(mockReqRes());
-
-      return expect(action.run.bind(action)).to.throw();
-    });
-
-    it('should should render the returned value of a filter (if that value != null)', function() {
-      let rendered;
-      class TestAction extends Action {
-        static before = [ 'preempt' ];
-        respond() {
-          return { never: 'reached' };
-        }
-        preempt() {
-          return { hello: 'world' };
-        }
-        render(result) {
-          rendered = result;
-        }
-      }
-      let action = new TestAction(mockReqRes());
-
-      return action.run().then(() => {
-        expect(rendered).to.eql({ hello: 'world' });
-      });
-    });
-
-  });
-
-  describe('content negotiation', function() {
-
-    it('should respond with the content-type specific responder', function() {
-      let responded = false;
-      let respondedWithHtml = false;
-      class TestAction extends Action {
-        respond() {
-          responded = true;
-        }
-        respondWithHtml() {
-          respondedWithHtml = true;
-        }
-      }
-      let action = new TestAction(mockReqRes({
-        request: {
-          headers: {
-            'Content-type': 'text/html'
-          },
-          accepts() {
-            return 'html';
-          }
-        }
-      }));
-
-      return action.run().then(() => {
-        expect(responded).to.be.false();
-        expect(respondedWithHtml).to.be.true();
-      });
-    });
-
-  });
-
-  describe('when render is not called', function() {
-
-    it('should autorender', function() {
-      let rendered;
-      class TestAction extends Action {
-        respond() {
-          return new Response(200, { foo: 'bar' }, { raw: true });
-        }
-      }
-      let action = new TestAction(mockReqRes({
-        response: {
-          write(result) {
-            rendered = JSON.parse(result);
-          }
-        }
-      }));
-
-      return action.run().then(() => {
-        expect(rendered).to.eql({ foo: 'bar' });
-      });
-    });
-
-  });
-
-  describe('wrapper methods', function() {
-
-    it('should return model when using modelFor', function() {
-      let model;
-      class TestAction extends Action {
-        respond() {
-          let User = this.modelFor('user');
-          model = User;
-          return User;
-        }
-      }
-      let mock = mockReqRes();
-
-      mock.container.register('model:user', { type: 'user' });
-
-      let action = new TestAction(mock);
-
-      return action.run().then(() => {
-        expect(model.type).to.eql('user');
-        expect(model.container).to.eql(action.container);
-      });
-    });
-
-    it('should return service when using service(type)', function() {
-      let service;
-      class TestAction extends Action {
-        respond() {
-          let Mine = this.service('mine');
-          service = Mine;
-          return Mine;
-        }
-      }
-      let mock = mockReqRes();
-
-      mock.container.register('service:mine', { type: 'mine' });
-
-      let action = new TestAction(mock);
-
-      return action.run().then(() => {
-        expect(service.type).to.eql('mine');
-      });
-    });
-
-  });
-
+  let service = action.service('mine');
+  t.is(service.type, 'mine');
 });
--- test/unit/container-test.js
+++ test/unit/container-test.js
@@ -1,108 +1,79 @@
-import expect from 'must';
-import Container from '../../lib/runtime/container';
+import test from 'ava';
+import { Container } from 'denali';
 
-describe('Denali.Container', function() {
-
-  describe('registering', function() {
-    it('should register a value', function() {
-      let container = new Container();
-      container.register('foo:bar', { buzz: true });
-      expect(container.lookup('foo:bar').buzz).to.be.true();
-    });
-  });
-
-  describe('lookup', function() {
-
-    it('should lookup a module', function() {
-      let container = new Container();
-      container.register('foo:bar', { buzz: true });
-      expect(container.lookup('foo:bar').buzz).to.be.true();
-    });
-
-    describe('lookupAll("type")', function() {
-
-      it('should return an object with all the modules of the given type', function() {
-        let container = new Container();
-        container.register('foo:bar', { buzz: true });
-        container.register('foo:buzz', { bat: true });
-        let type = container.lookupAll('foo');
-        expect(type.bar).to.be.exist();
-        expect(type.bar.buzz).to.be.true();
-        expect(type.buzz).to.be.exist();
-        expect(type.buzz.bat).to.be.true();
-      });
-
-    });
-
-    describe('singletons', function() {
-
-      it('should instantiate a singleton', function() {
-        let container = new Container();
-        class Class {
-          static singleton = true
-        }
-        container.register('foo:bar', new Class());
-
-        let classInstance = container.lookup('foo:bar');
-        let classInstanceTwo = container.lookup('foo:bar');
-        expect(classInstance).to.be.an.instanceof(Class);
-        expect(classInstanceTwo).to.be(classInstance);
-      });
-
-      it('should lazily instantiate (i.e. on lookup)', function() {
-        let container = new Container();
-        let instantiated = false;
-        function Class() {
-          instantiated = true;
-        }
-        Class.singleton = true;
-        container.register('foo:bar', Class);
-
-        expect(instantiated).to.be.false();
-      });
-
-    });
-
-    describe('lookupSerializer', function() {
-
-      it('should inject all serializer singletons into each serializer', function() {
-        let container = new Container();
-        class SerializerOne {
-          static singleton = true
-        }
-        class SerializerTwo {
-          static singleton = true
-        }
-        container.register('serializer:one', new SerializerOne());
-        container.register('serializer:two', new SerializerTwo());
-
-        let serializerOne = container.lookup('serializer:one');
-        expect(serializerOne).to.be.an.instanceof(SerializerOne);
-        // expect(serializerOne.serializers).to.have.keys([ 'one', 'two' ]);
-        // expect(serializerOne.serializers.two).to.be.an.instanceof(SerializerTwo);
-      });
-
-    });
-
-    describe('lookupAdapter', function() {
-
-      it('should inject all adapter singletons into each adapter', function() {
-        let container = new Container();
-        class AdapterOne {
-          static singleton = true
-        }
-        class AdapterTwo {
-          static singleton = true
-        }
-        container.register('adapter:one', new AdapterOne());
-        container.register('adapter:two', new AdapterTwo());
-
-        let adapterOne = container.lookup('adapter:one');
-        expect(adapterOne).to.be.an.instanceof(AdapterOne);
-        // expect(adapterOne.adapters).to.have.keys([ 'one', 'two' ]);
-        // expect(adapterOne.adapters.two).to.be.an.instanceof(AdapterTwo);
-      });
-
-    });
-  });
+test('Container > #register(type, value) > registers a value on the container', async (t) => {
+  let container = new Container();
+  container.register('foo:bar', { buzz: true });
+  t.true(container.lookup('foo:bar').buzz);
 });
+
+test('Container > #lookup(type) > looks up a module', async (t) => {
+  let container = new Container();
+  container.register('foo:bar', { buzz: true });
+  t.true(container.lookup('foo:bar').buzz);
+});
+
+test('Container > #lookupAll(type) > returns an object with all the modules of the given type', async (t) => {
+  let container = new Container();
+  container.register('foo:bar', { buzz: true });
+  container.register('foo:buzz', { bat: true });
+  let type = container.lookupAll('foo');
+  t.truthy(type.bar);
+  t.true(type.bar.buzz);
+  t.truthy(type.buzz);
+  t.true(type.buzz.bat);
+});
+
+test('Container > singletons > should instantiate a singleton', async (t) => {
+  let container = new Container();
+  class Class {
+    static singleton = true
+  }
+  container.register('foo:bar', new Class());
+
+  let classInstance = container.lookup('foo:bar');
+  let classInstanceTwo = container.lookup('foo:bar');
+  t.true(classInstance instanceof Class);
+  t.is(classInstanceTwo, classInstance);
+});
+
+test('Container > singletons > lazily instantiates singletons (i.e. on lookup)', async (t) => {
+  let container = new Container();
+  function Class() {
+    t.fail('Class should not have been instantiated.');
+  }
+  Class.singleton = true;
+  container.register('foo:bar', Class);
+});
+
+test.todo('Container > #lookupSerializer() > injects all serializer singletons into each serializer');
+// let container = new Container();
+// class SerializerOne {
+//   static singleton = true
+// }
+// class SerializerTwo {
+//   static singleton = true
+// }
+// container.register('serializer:one', new SerializerOne());
+// container.register('serializer:two', new SerializerTwo());
+//
+// let serializerOne = container.lookup('serializer:one');
+// expect(serializerOne).to.be.an.instanceof(SerializerOne);
+// expect(serializerOne.serializers).to.have.keys([ 'one', 'two' ]);
+// expect(serializerOne.serializers.two).to.be.an.instanceof(SerializerTwo);
+
+test.todo('Container > #lookupAdapter > injects all adapter singletons into each adapter');
+// let container = new Container();
+// class AdapterOne {
+//   static singleton = true
+// }
+// class AdapterTwo {
+//   static singleton = true
+// }
+// container.register('adapter:one', new AdapterOne());
+// container.register('adapter:two', new AdapterTwo());
+//
+// let adapterOne = container.lookup('adapter:one');
+// expect(adapterOne).to.be.an.instanceof(AdapterOne);
+// expect(adapterOne.adapters).to.have.keys([ 'one', 'two' ]);
+// expect(adapterOne.adapters.two).to.be.an.instanceof(AdapterTwo);
