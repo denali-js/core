13941407c65bd4f450a5e49da3af1bd62e884c5c
Dave Wasmer <davewasmer@gmail.com>
2016-10-23 22:17:28 -0600

    test(cli): add test for server command inside addons

--- test/acceptance/commands/server-test.js
+++ test/acceptance/commands/server-test.js
@@ -1,8 +1,27 @@
 import test from 'ava';
+import path from 'path';
 import { CommandAcceptanceTest } from 'denali';
 
 test('server command > launches a server', async () => {
   let server = new CommandAcceptanceTest('server');
+
+  return server.spawn({
+    env: {
+      DENALI_ENV: 'development'
+    },
+    checkOutput(stdout) {
+      return stdout.indexOf('dummy@0.0.0 server up') > -1;
+    }
+  });
+});
+
+test('server command > launches a server based on the dummy app in an addon', async () => {
+  let generateAddon = new CommandAcceptanceTest('addon foobar', { populateWithDummy: false });
+  await generateAddon.run();
+  let server = new CommandAcceptanceTest('server', {
+    dir: path.join(generateAddon.dir, 'foobar'),
+    populateWithDummy: false
+  });
 
   return server.spawn({
     env: {
