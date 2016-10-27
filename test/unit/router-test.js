bde2c746fbd2a03cb64b3fe8aa9aa06358e4abe5
Dave Wasmer <davewasmer@gmail.com>
2016-10-26 20:37:39 -0600

    fix(runtime): do not fallback to application serializer when action.serializer = false; closes #161 (#192)

--- test/unit/router-test.js
+++ test/unit/router-test.js
@@ -25,3 +25,28 @@ test('Router > runs middleware before determining routing', async (t) => {
   });
   await router.handle(new MockRequest(), new MockResponse());
 });
+
+test('Router > does not attempt to serialize when action.serializer = false', async (t) => {
+  t.plan(1);
+  let container = new Container();
+  container.register('action:index', class TestAction extends Action {
+    serializer = false;
+    respond() {
+      t.pass();
+    }
+  });
+  container.register('serializer:application', {
+    parse() {
+      t.fail('Router should not have attempted to parse this request with a serializer');
+    },
+    serialize() {
+      t.fail('Router should not have attempted to serialize this response with a serializer');
+    }
+  });
+  let router = new Router({ container });
+  router.post('/', 'index');
+
+  let req = new MockRequest({ url: '/', method: 'POST' });
+  req.write('{}');
+  await router.handle(req, new MockResponse());
+});
