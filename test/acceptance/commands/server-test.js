f6e75383d0efa32993987ae66e5a5337898e62c0
Dave Wasmer <davewasmer@gmail.com>
2016-10-28 15:51:17 -0600

    fix: remove npmignore and package.json "files"

--- test/acceptance/commands/server-test.js
+++ test/acceptance/commands/server-test.js
@@ -2,20 +2,24 @@ import test from 'ava';
 import path from 'path';
 import { CommandAcceptanceTest } from 'denali';
 
-test('server command > launches a server', async () => {
+test('server command > launches a server', async (t) => {
   let server = new CommandAcceptanceTest('server --port 3001');
 
   return server.spawn({
     env: {
       DENALI_ENV: 'development'
     },
-    checkOutput(stdout) {
+    checkOutput(stdout, stderr) {
+      if (stderr.length > 0) {
+        t.fail(stderr);
+        return true;
+      }
       return stdout.indexOf('dummy@0.0.0 server up') > -1;
     }
   });
 });
 
-test('server command > launches a server based on the dummy app in an addon', async () => {
+test('server command > launches a server based on the dummy app in an addon', async (t) => {
   let generateAddon = new CommandAcceptanceTest('addon foobar --use-npm', { populateWithDummy: false });
   await generateAddon.run();
   let server = new CommandAcceptanceTest('server --port 3002', {
@@ -27,7 +31,11 @@ test('server command > launches a server based on the dummy app in an addon', as
     env: {
       DENALI_ENV: 'development'
     },
-    checkOutput(stdout) {
+    checkOutput(stdout, stderr) {
+      if (stderr.length > 0) {
+        t.fail(stderr);
+        return true;
+      }
       return stdout.indexOf('dummy@0.0.0 server up') > -1;
     }
   });
