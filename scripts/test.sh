884b045b984c77266a15e0b74123a5d1d842d3ed
Dave Wasmer <davewasmer@gmail.com>
2016-11-27 17:00:13 -0700

    chore: exclude unneeded files in test rsync script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 set -e
 rm -rf tmp
 rm -rf node_modules/denali
-rsync -aq --progress . node_modules/denali --exclude node_modules --exclude .git
+rsync -aq --progress . node_modules/denali --exclude node_modules --exclude .git --exclude tmp --exclude dist --exclude .nyc_output
 NODE_ENV=test ./bin/denali test "$@"
