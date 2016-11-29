a8ad630533d61172829180ac63051e8a32f57c54
Dave Wasmer <davewasmer@gmail.com>
2016-11-29 16:24:56 -0700

    feat(cli): add failOnStderr option to CommandAcceptanceTest.run()

--- test/acceptance/commands/routes-test.js
+++ test/acceptance/commands/routes-test.js
@@ -5,7 +5,7 @@ import { CommandAcceptanceTest } from 'denali';
 test('routes command > prints list of configured routes', async (t) => {
   let generate = new CommandAcceptanceTest('routes');
 
-  let result = await generate.run();
+  let result = await generate.run({ failOnStderr: true });
   t.is(result.stdout.trim(), dedent`
 ┌───────┬────────┐
 │ URL   │ ACTION │
