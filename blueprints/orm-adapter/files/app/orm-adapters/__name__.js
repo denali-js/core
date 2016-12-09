6bcc07ce9647ce8c414431edf72c93ed1a5d730c
Dave Wasmer <davewasmer@gmail.com>
2016-12-08 18:13:13 -0700

    feat(blueprint): add orm-adapter blueprint

---
+++ blueprints/orm-adapter/files/app/orm-adapters/__name__.js
@@ -0,0 +1,61 @@
+import { ORMAdapter } from 'denali';
+
+export default class <%= className %>Adapter extends ORMAdapter {
+
+  async find(type, query, options) {
+
+  }
+
+  async createRecord(type, data, options) {
+
+  }
+
+  buildRecord(type, data, options) {
+
+  }
+
+  idFor(model) {
+
+  }
+
+  getAttribute(model, property) {
+
+  }
+
+  setAttribute(model, property, value) {
+
+  }
+
+  deleteAttribute(model, property) {
+
+  }
+
+  async getRelated(model, relationship) {
+
+  }
+
+  async setRelated(model, relationship, relatedRecords) {
+
+  }
+
+  async addRelated(model, relationship, relatedRecord) {
+
+  }
+
+  async removeRelated(model, relationship, relatedRecord) {
+
+  }
+
+  async saveRecord(model) {
+
+  }
+
+  async deleteRecord(model) {
+
+  }
+
+  async defineModels(models) {
+
+  }
+
+}
