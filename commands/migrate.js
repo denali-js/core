7df3e4d7d3abb107dbff2ec4f80a4cb9909374d8
Dave Wasmer <davewasmer@gmail.com>
2017-01-05 20:46:16 -0700

    feat(cli): add migrate command

---
+++ commands/migrate.js
@@ -0,0 +1,49 @@
+import dedent from 'dedent-js';
+import Command from '../lib/cli/command';
+import Project from '../lib/cli/project';
+import knex from 'knex';
+
+export default class MigrateCommand extends Command {
+
+  static commandName = 'migrate';
+  static description = 'Run migrations to update your database schema';
+  static longDescription = dedent`
+    Runs (or rolls back) schema migrations for your database. Typically only
+    applies when use SQL-based databases.`;
+
+  flags = {
+    rollback: {
+      description: 'Rollback the latest migration, or latest --step migrations. Defaults to 1 step.',
+      defaultValue: false,
+      type: Boolean
+    },
+    redo: {
+      description: 'Shortcut for rolling back then migrating up again. If used with --step, it will replay that many migrations. If used with --version, it will roll back to that version then replay. If neither, defaults to --step 1',
+      defaultValue: false,
+      type: Boolean
+    }
+  };
+
+  runsInApp = true;
+
+  allowExtraArgs = false;
+
+  async run({ flags }) {
+    let project = new Project({
+      environment: flags.environment,
+      printSlowTrees: flags['print-slow-trees'],
+      buildDummy: true
+    });
+    let application = await project.createApplication();
+    let db = knex(application.config.migrations.db);
+    if (flags.rollback) {
+      await db.migrate.rollback();
+    } else if (flags.redo) {
+      await db.migrate.rollback();
+      await db.migrate.latest();
+    } else {
+      await db.migrate.latest();
+    }
+  }
+
+}
