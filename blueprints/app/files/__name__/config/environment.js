c123a79f2254019ba72dd53606320309b7047933
Dave Wasmer <davewasmer@gmail.com>
2017-01-11 18:46:38 -0700

    docs: flesh out inline comments for migration config in environment.js

--- blueprints/app/files/__name__/config/environment.js
+++ blueprints/app/files/__name__/config/environment.js
@@ -3,6 +3,17 @@ export default function environmentConfig(environment) {
     server: {
       port: process.env.PORT || 3000
     },
+    // == Migrations
+    //
+    // If you are planning on using migrations with your database, uncomment this
+    // section and provide the relevant database driver and connection details
+    //
+    // migrations: {
+    //   db: {
+    //     client: 'pg',
+    //     connection: {}
+    //   }
+    // },
     ormAdapter: 'memory',
     database: {
 
@@ -16,19 +27,21 @@ export default function environmentConfig(environment) {
   if (environment === 'production') {
     // production-specific config
 
+    // == SSL
+    //
     // You can start Denali in SSL mode by providing your private key and
     // certificate, or your pfx file contents
     //
-    //   config.server.ssl = {
-    //     key: fs.readFileSync('privatekey.pem'),
-    //     cert: fs.readFileSync('certificate.pem')
-    //   };
+    // config.server.ssl = {
+    //   key: fs.readFileSync('privatekey.pem'),
+    //   cert: fs.readFileSync('certificate.pem')
+    // };
     //
     // or,
     //
-    //   config.server.ssl = {
-    //     pfx: fs.readFileSync('server.pfx')
-    //   };
+    // config.server.ssl = {
+    //   pfx: fs.readFileSync('server.pfx')
+    // };
     //
   }
 
