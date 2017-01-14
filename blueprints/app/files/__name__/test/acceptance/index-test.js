d4dc1ffbfd7d9c0a8935730b57a8d6c265d6294b
Dave Wasmer <davewasmer@gmail.com>
2017-01-14 15:02:17 -0700

    fix(blueprint): t.equal -> t.is in app blueprint

--- blueprints/app/files/__name__/test/acceptance/index-test.js
+++ blueprints/app/files/__name__/test/acceptance/index-test.js
@@ -4,5 +4,5 @@ import { AppAcceptanceTest } from 'denali';
 test('GET / > should return a welcome message', async (t) => {
   let app = new AppAcceptanceTest();
   let { body } = await app.get('/');
-  t.equal(body.message, 'Welcome to Denali!');
+  t.is(body.message, 'Welcome to Denali!');
 });
