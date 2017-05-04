c2657ac494d739e8bab2bf36c360ef591d30a1c0
Dave Wasmer <davewasmer@gmail.com>
2017-05-03 20:57:14 -0600

    WIP

--- lib/metal/container.ts
+++ lib/metal/container.ts
@@ -1,213 +1,25 @@
 import {
-  camelCase,
-  forOwn,
-  isObject,
-  forEach,
-  defaults,
-  forIn
+  uniq
 } from 'lodash';
-import * as dedent from 'dedent-js';
-import { Dict, Constructor } from '../utils/types';
-import DenaliObject from './object';
+import { Container as GlimmerContainer, FactoryDefinition } from '@glimmer/di';
 import Resolver from './resolver';
-import { assign, mapValues } from 'lodash';
+import Registry from './registry';
 
-export interface ParsedName {
-  fullName: string;
-  type: string;
-  modulePath: string;
-  moduleName: string;
-}
+export default class Container extends GlimmerContainer {
 
-export interface ContainerOptions {
-  containerize?: boolean;
-  singleton?: boolean;
-}
+  lookupAll(type: string) {
+    // Get around the private keyword from glimmer
+    let resolver = <Resolver>(<any>this)._resolver;
+    let registry = <Registry>(<any>this)._registry;
 
-const DEFAULT_CONTAINER_OPTIONS = {
-  containerize: true,
-  singleton: false
-}
+    let resolvedEntries = resolver.availableForType(type);
+    let registeredEntries = registry.availableForType(type);
 
-
-/**
- * The Container houses all the various classes that makeup a Denali app's
- *
- * runtime. It holds references to the modules themselves, as well as managing lookup logic (i.e.
- * some types of classes fall back to a generic "application" class if a more specific one is not
- * found.
- *
- * @package runtime
- * @since 0.1.0
- */
-export default class Container extends DenaliObject {
-
-  constructor(root: string | Resolver = process.cwd()) {
-    super();
-    this.resolvers.push(typeof root === 'string' ? new Resolver(root) : root);
-  }
-
-  /**
-   * An internal cache of lookups and their resolved values
-   */
-  private lookups: Map<string, any> = new Map();
-
-  /**
-   * Optional resolvers to use as a fallback if the default resolver is unable to resolve a lookup.
-   * Usually these are the resolvers for child addons, but you could also use a fallback resolver
-   * to support an alternative directory structure for your app. NOTE: this is NOT recommended, and
-   * may break compatibility with poorly designed addons as well as certainly CLI features.
-   */
-  private resolvers: Resolver[] = [];
-
-  /**
-   * Holds options for how to handle constructing member objects
-   */
-  private memberOptions: Map<string, ContainerOptions> = new Map([
-    [ 'app', { singleton: true } ],
-    [ 'orm-adapter', { singleton: true } ],
-    [ 'serializer', { singleton: true } ],
-    [ 'service', { singleton: true } ]
-  ]);
-
-  /**
-   * Add a fallback resolver to the bottom of the fallback queue.
-   *
-   * @since 0.1.0
-   */
-  public addResolver(resolver: Resolver) {
-    this.resolvers.push(resolver);
-  }
-
-  /**
-   * Register a value under the given `fullName` for later use.
-   *
-   * @since 0.1.0
-   */
-  public register(name: string, value: any, options?: ContainerOptions): void {
-    this.resolvers[0].register(name, value);
-    if (options) {
-      this.registerOptions(name, options);
-    }
-  }
-
-  /**
-   * Set options for how the given member will be constructed. Options passed in are merged with any
-   * existing options - they do not replace them entirely.
-   *
-   * @since 0.1.0
-   */
-  public registerOptions(name: string, options: ContainerOptions = {}): void {
-    let { fullName } = parseName(name);
-    let currentOptions = this.memberOptions.get(fullName);
-    this.memberOptions.set(fullName, assign(currentOptions, options));
-  }
-
-  /**
-   * Get the given option for the given member of the container
-   *
-   * @since 0.1.0
-   */
-  public optionFor(name: string, option?: keyof ContainerOptions): any {
-    let { type, fullName } = parseName(name);
-    let options = this.memberOptions.get(fullName);
-    let typeOptions = this.memberOptions.get(type);
-    let combinedOptions = defaults(defaults(options, typeOptions), DEFAULT_CONTAINER_OPTIONS);
-    if (!option) {
-      return combinedOptions;
-    }
-    return combinedOptions[option];
-  }
-
-  /**
-   * Lookup a value in the container. Uses type specific lookup logic if available.
-   *
-   * @since 0.1.0
-   */
-  public lookup(name: string, lookupOptions: { loose?: boolean, raw?: boolean } = {}): any {
-    let parsedName = parseName(name);
-
-    if (!this.lookups.has(parsedName.fullName)) {
-
-      // Find the member with the top level resolver
-      let object: any;
-      forEach(this.resolvers, (resolver) => {
-        object = resolver.retrieve(parsedName);
-        return !object; // Break loop if we found something
-      });
-
-      // Handle a bad lookup
-      if (!object) {
-        // Allow failed lookups (opt-in)
-        if (lookupOptions.loose) {
-          this.lookups.set(parsedName.fullName, null);
-          return null;
-        }
-        throw new Error(`No such ${ parsedName.type } found: '${ parsedName.moduleName }'`);
-      }
-
-      // Make the singleton if needed
-      if (this.optionFor(parsedName.fullName, 'singleton')) {
-        object = new object();
-      }
-
-      // Inject container references
-      if (this.optionFor(parsedName.fullName, 'containerize')) {
-        object.container = this;
-        if (object.prototype) {
-          object.prototype.container = this;
-        }
-      }
-
-      // Freeze the actual containered value to avoid allowing mutations. If `object` here is the
-      // direct result of a require() call, then any mutations to it will be shared with other
-      // containers that require() it (i.e. when running concurrent tests). If it's a singleton,
-      // this isn't necessary since each container gets it's own instance. But if it's not, then we
-      // freeze it to prevent mutations which can lead to extremely difficult to trace bugs.
-      if (!this.optionFor(parsedName.fullName, 'singleton')) {
-        object = Object.freeze(object);
-      }
-
-      this.lookups.set(parsedName.fullName, object);
-    }
-
-    return this.lookups.get(parsedName.fullName);
-  }
-
-  /**
-   * Lookup all modules of a specific type in the container. Returns an object of all the modules
-   * keyed by their module path (i.e. `role:employees/manager` would be found under
-   * `lookupAll('role')['employees/manager']`
-   */
-  public lookupAll(type: string): { [modulePath: string]: any } {
-    let resolverResultsets = this.resolvers.map((resolver) => {
-      return resolver.retrieveAll(type);
+    let typeMap: { [entry: string]: FactoryDefinition<any> } = {};
+    uniq(resolvedEntries.concat(registeredEntries)).map((specifier) => {
+      typeMap[specifier] = this.lookup(specifier);
     });
-    let mergedResultset = <{ [modulePath: string]: any }>(<any>assign)(...resolverResultsets.reverse());
-    return mapValues(mergedResultset, (rawResolvedObject, modulePath) => {
-      return this.lookup(`${ type }:${ modulePath }`);
-    });
+    return typeMap;
   }
 
-  /**
-   * For a given type, returns the names of all the available modules under that
-   * type. Primarily used for debugging purposes (i.e. to show available modules
-   * when a lookup of that type fails).
-   */
-  availableForType(type: string): string[] {
-    return Object.keys(this.lookupAll(type));
-  }
-}
-
-/**
- * Take the supplied name which can come in several forms, and normalize it.
- */
-export function parseName(name: string): ParsedName {
-  let [ type, modulePath ] = name.split(':');
-  return {
-    fullName: name,
-    type,
-    modulePath,
-    moduleName: camelCase(modulePath)
-  };
 }
--- lib/metal/resolver.ts
+++ lib/metal/resolver.ts
@@ -1,139 +1,101 @@
 import {
-  camelCase,
   upperFirst,
+  camelCase,
   omitBy
 } from 'lodash';
-import * as path from 'path';
-import * as fs from 'fs';
-import { parseName, ParsedName, ContainerOptions } from './container';
+import { Resolver as GlimmerResolver, FactoryDefinition } from '@glimmer/di';
 import * as tryRequire from 'try-require';
+import * as path from 'path';
 import { pluralize } from 'inflection';
+import * as fs from 'fs';
 import requireDir from '../utils/require-dir';
 
 interface RetrieveMethod {
-  (parsedName: ParsedName): any;
+  (type: string, entry: string): FactoryDefinition<any>;
 }
 
-export interface RetrieveAllMethod {
-  (type: string): { [modulePath: string]: any };
+interface AvailableForTypeMethod {
+  (type: string): string[];
 }
 
-export type Registry = Map<string, any>;
-
-export default class Resolver {
+export default class Resolver implements GlimmerResolver {
 
   [key: string]: any;
 
-  /**
-   * The root directory for this resolver to start from when searching for files
-   */
-  root: string;
-
-  /**
-   * The internal cache of available references
-   */
-  private registry: Registry = new Map();
-
-  constructor(root: string) {
-    this.root = root;
+  identify(specifier: string, referrer?: string) {
+    return '';
   }
 
-  /**
-   * Manually add a member to this resolver. Manually registered members take precedence over any
-   * retrieved from the filesystem.
-   */
-  public register(name: string, value: any) {
-    this.registry.set(parseName(name).fullName, value);
-  }
-
-  /**
-   * Fetch the member matching the given parsedName. First checks for any manually registered
-   * members, then falls back to type specific retrieve methods that typically find the matching
-   * file on the filesystem.
-   */
-  public retrieve(parsedName: ParsedName | string) {
-    if (typeof parsedName === 'string') {
-      parsedName = parseName(parsedName);
-    }
-    if (this.registry.has(parsedName.fullName)) {
-      return this.registry.get(parsedName.fullName);
-    }
-    let retrieveMethod = <RetrieveMethod>this[`retrieve${ upperFirst(camelCase(parsedName.type)) }`];
+  retrieve(specifier: string): FactoryDefinition<any> {
+    let [ type, entry ] = specifier.split(':');
+    let retrieveMethod = <RetrieveMethod>this[`retrieve${ upperFirst(camelCase(type)) }`];
     if (!retrieveMethod) {
       retrieveMethod = this.retrieveOther;
     }
-    let result = retrieveMethod.call(this, parsedName);
+    let result = retrieveMethod.call(this, type, entry);
     return result && result.default || result;
   }
 
   /**
    * Unknown types are assumed to exist underneath the `app/` folder
    */
-  protected retrieveOther(parsedName: ParsedName) {
-    return tryRequire(path.join(this.root, 'app', pluralize(parsedName.type), parsedName.modulePath));
+  protected retrieveOther(type: string, entry: string) {
+    return tryRequire(path.join(this.root, 'app', pluralize(type), entry));
   }
 
   /**
    * App files are found in `app/*`
    */
-  protected retrieveApp(parsedName: ParsedName) {
-    return tryRequire(path.join(this.root, 'app', parsedName.modulePath));
+  protected retrieveApp(type: string, entry: string) {
+    return tryRequire(path.join(this.root, 'app', entry));
   }
 
   /**
    * Config files are found in `config/`
    */
-  protected retrieveConfig(parsedName: ParsedName) {
-    return tryRequire(path.join(this.root, 'config', parsedName.modulePath));
+  protected retrieveConfig(type: string, entry: string) {
+    return tryRequire(path.join(this.root, 'config', entry));
   }
 
   /**
    * Initializer files are found in `config/initializers/`
    */
-  protected retrieveInitializer(parsedName: ParsedName) {
-    return tryRequire(path.join(this.root, 'config', 'initializers', parsedName.modulePath));
+  protected retrieveInitializer(type: string, entry: string) {
+    return tryRequire(path.join(this.root, 'config', 'initializers', entry));
   }
 
   /**
    * Retrieve all the members for a given type. First checks for all manual registrations matching
    * that type, then retrieves all members for that type (typically from the filesystem).
    */
-  public retrieveAll(type: string) {
-    let manualRegistrations: { [modulePath: string]: any } = {};
-    this.registry.forEach((entry, fullName) => {
-      let parsedName = parseName(fullName);
-      if (parsedName.type === type) {
-        manualRegistrations[parsedName.modulePath] = entry;
-      }
-    });
-    let retrieveMethod = <RetrieveAllMethod>this[`retrieveAll${ upperFirst(camelCase(type)) }`];
-    if (!retrieveMethod) {
-      retrieveMethod = this.retrieveAllOther;
+  public availableForType(type: string) {
+    let availableMethod = <AvailableForTypeMethod>this[`availableFor${ upperFirst(camelCase(type)) }`];
+    if (!availableMethod) {
+      availableMethod = this.availableForOther;
     }
-    let resolvedMembers = <{ [modulePath: string]: any }>retrieveMethod.call(this, type);
-    return Object.assign(resolvedMembers, manualRegistrations);
+    return (<string[]>availableMethod.call(this, type)).map((entry) => `${ type }:${ entry}`);;
   }
 
   /**
    * Unknown types are assumed to exist in the `app/` folder
    */
-  protected retrieveAllOther(type: string) {
+  protected availableForOther(type: string) {
     let typeDir = path.join(this.root, 'app', pluralize(type));
     if (fs.existsSync(typeDir)) {
-      return requireDir(typeDir);
+      return Object.keys(requireDir(typeDir));
     }
-    return {};
+    return [];
   }
 
   /**
    * App files are found in `app/*`
    */
-  protected retrieveAllApp(parsedName: ParsedName) {
+  protected retrieveAllApp(type: string) {
     let appDir = path.join(this.root, 'app');
     if (fs.existsSync(appDir)) {
-      return requireDir(appDir, { recurse: false });
+      return Object.keys(requireDir(appDir, { recurse: false }));
     }
-    return {};
+    return [];
   }
 
   /**
@@ -142,11 +104,12 @@ export default class Resolver {
   protected retrieveAllConfig(type: string) {
     let configDir = path.join(this.root, 'config');
     if (fs.existsSync(configDir)) {
-      return omitBy(requireDir(configDir), (mod, modulePath) => {
-        return modulePath.startsWith('initializers');
+      let entries = Object.keys(requireDir(configDir));
+      return entries.filter((entry) => {
+        return !entry.startsWith('initializers');
       });
     }
-    return {};
+    return [];
   }
 
   /**
@@ -155,9 +118,9 @@ export default class Resolver {
   protected retrieveAllInitializer(type: string) {
     let initializersDir = path.join(this.root, 'config', 'initializers');
     if (fs.existsSync(initializersDir)) {
-      return requireDir(initializersDir);
+      return Object.keys(requireDir(initializersDir));
     }
-    return {};
+    return [];
   }
 
 }
