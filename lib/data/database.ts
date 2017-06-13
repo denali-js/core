d2c77b162835761398a542f32fe929ff71889643
Austin Burdine <acburdine@gmail.com>
2017-06-13 18:47:58 -0400

    feat(database): pass through additional query options to model methods (#330)
    closes #329

--- lib/data/database.ts
+++ lib/data/database.ts
@@ -3,28 +3,28 @@ import Model from './model';
 
 export default class DatabaseService extends Service {
 
-  find(modelType: string, id: any): Promise<Model|null> {
+  find(modelType: string, id: any, options?: any): Promise<Model|null> {
     let ModelClass = <typeof Model>this.container.factoryFor(`model:${ modelType }`).class;
-    return ModelClass.find(this.container, id);
+    return ModelClass.find(this.container, id, options);
   }
 
-  queryOne(modelType: string, query: any): Promise<Model|null> {
+  queryOne(modelType: string, query: any, options?: any): Promise<Model|null> {
     let ModelClass = <typeof Model>this.container.factoryFor(`model:${ modelType }`).class;
-    return ModelClass.queryOne(this.container, query);
+    return ModelClass.queryOne(this.container, query, options);
   }
 
-  query(modelType: string, query: any): Promise<Model[]> {
+  query(modelType: string, query: any, options?: any): Promise<Model[]> {
     let ModelClass = <typeof Model>this.container.factoryFor(`model:${ modelType }`).class;
-    return ModelClass.query(this.container, query);
+    return ModelClass.query(this.container, query, options);
   }
 
-  all(modelType: string): Promise<Model[]> {
+  all(modelType: string, options?: any): Promise<Model[]> {
     let ModelClass = <typeof Model>this.container.factoryFor(`model:${ modelType }`).class;
-    return ModelClass.all(this.container);
+    return ModelClass.all(this.container, options);
   }
 
-  create(modelType: string, data: any): Model {
-    return this.container.factoryFor(`model:${ modelType }`).create(this.container, data);
+  create(modelType: string, data: any, options?: any): Model {
+    return this.container.factoryFor(`model:${ modelType }`).create(this.container, data, options);
   }
 
 }
