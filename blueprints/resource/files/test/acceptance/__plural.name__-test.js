730d1a126062b02cecd16e8a6a107ec7322f1dfb
Ilya Radchenko <knownasilya@gmail.com>
2017-06-21 11:18:29 -0400

    fix(blueprints): Make resource test comments two spaces, not 4 (#353)

--- blueprints/resource/files/test/acceptance/__plural.name__-test.js
+++ blueprints/resource/files/test/acceptance/__plural.name__-test.js
@@ -5,7 +5,7 @@ appAcceptanceTest(test);
 
 test('POST /<%= plural.dasherized %> > creates a <%= singular.humanized %>', async (t) => {
   let result = await t.context.app.post('/<%= plural.name %>', {
-      // Add the <%= singular.humanized %> payload here
+    // Add the <%= singular.humanized %> payload here
   });
 
   t.is(result.status, 201);
@@ -21,7 +21,7 @@ test('GET /<%= plural.dasherized %> > should list <%= plural.humanized %>', asyn
 
 test('GET /<%= plural.dasherized %>/:id > should show a <%= singular.humanized %>', async (t) => {
   let { body } = await t.context.app.post('/<%= plural.name %>', {
-      // Add the <%= singular.humanized %> payload here
+    // Add the <%= singular.humanized %> payload here
   });
   let id = body.data.id;
 
@@ -33,12 +33,12 @@ test('GET /<%= plural.dasherized %>/:id > should show a <%= singular.humanized %
 
 test('PATCH /<%= plural.dasherized %>/:id > should update a <%= singular.humanized %>', async (t) => {
   let { body } = await t.context.app.post('/<%= plural.name %>', {
-      // Add the <%= singular.humanized %> payload here
+    // Add the <%= singular.humanized %> payload here
   });
   let id = body.data.id;
 
   let result = await t.context.app.patch(`/<%= plural.name %>/${ id }`, {
-      // Add the <%= singular.humanized %> payload here
+    // Add the <%= singular.humanized %> payload here
   });
 
   t.is(result.status, 200);
@@ -47,7 +47,7 @@ test('PATCH /<%= plural.dasherized %>/:id > should update a <%= singular.humaniz
 
 test('DELETE /<%= plural.dasherized %>/:id > should delete a <%= singular.humanized %>', async (t) => {
   let { body } = await t.context.app.post('/<%= plural.name %>', {
-      // Add the <%= singular.humanized %> payload here
+    // Add the <%= singular.humanized %> payload here
   });
   let id = body.data.id;
 
