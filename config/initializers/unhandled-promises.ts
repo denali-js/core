b15630318382b782e24676ec3f71b76e5da9293e
Dave Wasmer <davewasmer@gmail.com>
2017-06-24 22:49:14 -0600

    feat: remove unhandled promise rejection handler to avoid potential memory leaks

--- config/initializers/unhandled-promises.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'unhandled-promises',
-  initialize(): void {
-    process.on('unhandledRejection', (reason: any) => {
-      // tslint:disable-next-line:no-console
-      console.error(reason.stack || reason.message || reason);
-    });
-  }
-};
