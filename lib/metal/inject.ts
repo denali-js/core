ea7d0e6ea7d2f616910d2685cebab8ea47a27f40
Dave Wasmer <davewasmer@gmail.com>
2017-07-03 00:25:04 -0600

    fix(metal): use global symbol for container injection flags to allow multiple denali versions to work with a single container

--- lib/metal/inject.ts
+++ lib/metal/inject.ts
@@ -5,7 +5,7 @@ export interface Injection {
   lookup: string;
 }
 
-export const IS_INJECTION = Symbol('container injection placeholder');
+export const IS_INJECTION = Symbol.for('denali:container:injection-placeholder');
 
 export function isInjection(value: any): value is Injection {
   return value != null && Boolean(value[IS_INJECTION]);
