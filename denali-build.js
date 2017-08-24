277ad8f14f6ad4234f227315abd217f78b81edda
Dave Wasmer <davewasmer@gmail.com>
2017-08-24 17:58:03 -0600

    fix: add missing imports to denali-build.js

--- denali-build.js
+++ denali-build.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const { Builder, ui } = require(`denali-cli`);
 const { exec } = require('child_process');
+const Funnel = require('broccoli-funnel');
+const MergeTree = require('broccoli-merge-trees');
+const { typescript: Typescript } = require('broccoli-typescript-compiler');
 
 module.exports = class DenaliBuilder extends Builder {
 
