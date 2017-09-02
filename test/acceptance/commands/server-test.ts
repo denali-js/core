84147c87da9562e1a32f984de06585b9e015e54e
Dave Wasmer <davewasmer@gmail.com>
2017-09-01 22:54:15 -0600

    refactor(test): simplify addon server command test

--- test/acceptance/commands/server-test.ts
+++ test/acceptance/commands/server-test.ts
@@ -4,7 +4,9 @@ import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as mkdirp from 'mkdirp';
 import * as rimraf from 'rimraf';
+import { execSync as run } from 'child_process';
 import { CommandAcceptanceTest } from 'denali-cli';
+import * as tmp from 'tmp';
 
 function linkDependency(pkgDir: string, dependencyName: string, dependencyDir: string) {
   let dest = path.join(pkgDir, 'node_modules', dependencyName);
@@ -33,18 +35,36 @@ test('launches a server', async (t) => {
 });
 
 test('launches a server based on the dummy app in an addon', async (t) => {
-  let generateAddon = new CommandAcceptanceTest('addon my-denali-addon', { name: 'server-command-dummy-app', populateWithDummy: false });
-  await generateAddon.run({ failOnStderr: true });
-  linkDependency(path.join(generateAddon.dir, 'my-denali-addon'), 'denali', path.join(process.cwd(), 'node_modules', 'denali'));
+  // Generate a new, blank addon. Do it outisde our normal tmp folder so that
+  // we don't hit problems with the addon blueprint thinking it's inside an
+  // existing project
+  let denaliBin = path.join(process.cwd(), 'node_modules', 'denali-cli', 'dist', 'bin', 'denali');
+  let dir = tmp.dirSync({
+    unsafeCleanup: true,
+    prefix: `denali-server-command-launches-dummy-app-from-inside-addon-`
+  }).name;
+  await run(`${ denaliBin } addon my-denali-addon`, { cwd: dir });
+  dir = path.join(dir, 'my-denali-addon');
+
+  // Use local copies of denali-cli and denali, not whatever npm installed. Copy,
+  // don't symlink, Denali itself. If it was symlinked, the addon would try to build
+  // Denali itself, which hits concurrency issues as other tests might be trying
+  // to access the built files as they are removed by the addon for rebuild. Copying
+  // Denali itself means the addon won't try to build it, and even if it does, will
+  // build it's own copy
+  linkDependency(dir, 'denali-cli', path.join(process.cwd(), 'node_modules', 'denali-cli'));
+  fs.removeSync(path.join(dir, 'node_modules', 'denali'));
+  fs.copySync(process.cwd(), path.join(dir, 'node_modules', 'denali'));
+
   let server = new CommandAcceptanceTest('server --port 3002', {
-    dir: path.join(generateAddon.dir, 'my-denali-addon'),
+    dir,
     populateWithDummy: false
   });
 
   return server.spawn({
     failOnStderr: true,
     checkOutput(stdout, stderr) {
-      let started = stdout.indexOf('dummy@0.0.0 server up') > -1;
+      let started = stdout.indexOf('my-denali-addon@0.0.1 server up') > -1;
       if (started) {
         t.pass();
       }
